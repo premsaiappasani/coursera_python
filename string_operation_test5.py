0b3d42f0b38d4b70b27931dd475bb8b2d3758300
premsaiappasani <55195257+premsaiappasani@users.noreply.github.com>
2021-01-07 22:54:55 +0530

    Create string_operation_test5.py

---
+++ string_operation_test5.py
@@ -0,0 +1,20 @@
+#The group_list function accepts a group name and a list of members, and returns a string with the format: group_name: member1, member2, … 
+#For example, group_list("g", ["a","b","c"]) returns "g: a, b, c".
+#Fill in the gaps in this function to do that.
+
+
+def group_list(group, users):
+  i=0
+  members= '"' + group + ':'
+  for user in users:
+    members += ' ' + user 
+    i+=1
+    if i==len(users):
+      return members+'"'
+    members+=","
+  return members + '"'
+  
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
