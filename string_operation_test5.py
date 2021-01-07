3e726bd859804725100b41e286c9493f26c11885
premsaiappasani <55195257+premsaiappasani@users.noreply.github.com>
2021-01-07 23:18:16 +0530

    Update string_operation_test5.py

--- string_operation_test5.py
+++ string_operation_test5.py
@@ -18,3 +18,17 @@ def group_list(group, users):
 print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
 print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
 print(group_list("Users", "")) # Should be "Users:"
+
+
+
+
+def group_list(group, users):
+  arr=""
+  for user in users:
+    arr= ", ".join(users)
+  members = ''.join(['"',group,": ",arr,'"'])
+  return members
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
